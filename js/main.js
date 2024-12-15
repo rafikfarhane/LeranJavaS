b0c6688fe6c79fa17b3ade73f977a79c09cb6bc4
Rafik <s94rfarh@uni-bonn.de>
2024-12-15 15:46:43 +0100

    technique to use private field in js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,51 @@
-let username = prompt("Please enter your name.")
-if (username){
-console.log(username);
+/* function pizzaFactory(pizzaSize){
+    const crust = "original";
+    const size = pizzaSize;
+    return {
+        bake: () => console.log(`Baking a ${size} ${crust} crust pizza.`)
+    };
 }
-else{
-    console.log("hey, enter your name");
+
+const myPizza = pizzaFactory("small");
+myPizza.bake(); */
+
+
+//Js Classes
+/* class Pizza{
+    constructor(pizzaSize){
+        this.size = pizzaSize;
+        this.crust = "original"
+    }
+    getCrust(){
+        return this.crust;
+    }
+    setCrust(crustToSet){
+        this.crust = crustToSet;
+    }
+    
+} */
+
+
+class Pizza{
+    crust = "original";
+    #sauce = "traditional";
+    #size;
+    constructor(pizzaSize){
+        this.#size = pizzaSize;
+    }
+    getCrust(){
+        return this.crust;
+    }
+    setCrust(crustToSet){
+        this.crust = crustToSet;
+    }
+
+    hereYouGo(){
+        console.log(`Here's yout ${this.crust} ${this.#sauce} sauce ${this.#size} pizza.`);
+    }
+    
 }
+
+const myPizza = new Pizza("large");
+myPizza.hereYouGo();
+console.log(myPizza.crust);
