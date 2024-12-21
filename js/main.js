95424748be7a9815519f7dc33975dd9ee7c05de6
Rafik <s94rfarh@uni-bonn.de>
2024-12-21 21:45:05 +0100

    the hierarchy in event liseners and the importance of event.target as a object and what it is refering to

--- js/main.js
+++ js/main.js
@@ -2,17 +2,6 @@
 
 // Syntax: addEventListener("click", function, useCapture)
 
-/* const doSomething = () => {
-    alert("doing something");
-}
-
-h2.addEventListener("click", doSomething, false);
-h2.removeEventListener("click", doSomething, false);
-
-h2.addEventListener("click", (event) => {
-    console.log(event.target);
-    event.target.textContent = "clicked";
-}) */
 
 document.addEventListener("readystatechange", (event) => {
     if(event.target.readyState === "complete") {
@@ -27,14 +16,17 @@ const initApp = () => {
     const h2 = div.querySelector("h2");
 
     view.addEventListener("click", (event) => {
-        view.style.backgroundColor = "purple";
+        
+        event.target.style.backgroundColor = "purple";
+
     });
 
     div.addEventListener("click", (event) => {
-        div.style.backgroundColor = "blue";
+        event.target.style.backgroundColor = "blue";
     });
 
     h2.addEventListener("click", (event) => {
+        
         event.target.textContent = "Clicked";
     });
 
