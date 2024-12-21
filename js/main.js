640e1ad48de3b224bdf31030e7dceb06c7ce42c1
Rafik <s94rfarh@uni-bonn.de>
2024-12-21 23:05:15 +0100

    using preventDefault for more controll

--- js/main.js
+++ js/main.js
@@ -11,36 +11,10 @@ document.addEventListener("readystatechange", (event) => {
 });
 
 const initApp = () => {
-    const view = document.querySelector("#view2");
-    const div = view.querySelector("div");
-    const h2 = div.querySelector("h2");
-
-    view.addEventListener("click", (event) => {
-        
-        view.classList.toggle("purple");
-        view.classList.toggle("darkblue");
-
+    const view3 = document.querySelector("#view3");
+    const myForm = view3.querySelector("#myForm");
+    myForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log("submit event");
     });
-
-    div.addEventListener("click", (event) => {
-        div.classList.toggle("blue");
-        div.classList.toggle("black");
-    });
-
-    h2.addEventListener("click", (event) => {
-        const myText = event.target.textContent;
-        myText === "My 2nd View" 
-        ? (event.target.textContent = "Clicked")
-        : (event.target.textContent = "My 2nd View");
-    });
-
-    const nav = document.querySelector("nav");
-    nav.addEventListener("mouseover", (event) => {
-        event.target.classList.add("height100");
-    });
-
-    nav.addEventListener("mouseout", (event) => {
-        event.target.classList.remove("height100");
-    })
-
 };
