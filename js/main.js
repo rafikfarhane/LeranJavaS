b8a8ec7ae6dffd936aec7a0bb9ad78070a20bc13
Rafik <s94rfarh@uni-bonn.de>
2024-12-21 17:46:39 +0100

    small interactive web-site implemented

--- js/main.js
+++ js/main.js
@@ -1,80 +1,41 @@
-//DOM - Document Object Model
 
-//selecting (html documents by id)
-const view1 = document.getElementById("view1");
-console.log(view1);
-const view2 = document.querySelector("#view2");
-console.log(view2);
 
-view1.style.display = "flex";
-view2.style.display = "none";
+// Syntax: addEventListener("click", function, useCapture)
 
-const views = document.getElementsByClassName("view");
-console.log(views);
-const sameViews = document.querySelectorAll('.view');
-console.log(sameViews);
-
-const divs = view1.querySelectorAll("div");
-console.log(divs);
-const sameDivs = view1.getElementsByTagName("div");
-console.log(sameDivs);
-
-const evenDivs = view1.querySelectorAll("div:nth-of-type(2n)");
-console.log(evenDivs);
-for (let i=0; i<evenDivs.length; i++){
-    evenDivs[i].style.backgroundColor = "darkblue";
+/* const doSomething = () => {
+    alert("doing something");
 }
 
-const navText = document.querySelector("nav h1");
-console.log(navText);
-navText.textContent = "Hello World!";
-const navbar = document.querySelector("nav");
-navbar.innerHTML = '<h1>Hello!</h1> <p>This should align right.';
+h2.addEventListener("click", doSomething, false);
+h2.removeEventListener("click", doSomething, false);
 
-console.log(navbar);
-navbar.style.justifyContent = "space-between";
+h2.addEventListener("click", (event) => {
+    console.log(event.target);
+    event.target.textContent = "clicked";
+}) */
 
-console.log(evenDivs[0]);
-console.log(evenDivs[0].parentElement);
-console.log(evenDivs[0].parentElement.children);
-console.log(evenDivs[0].parentElement.childNodes);
-console.log(evenDivs[0].parentElement.hasChildNodes());
-console.log(evenDivs[0].parentElement.lastChild);
-console.log(evenDivs[0].parentElement.lastElementChild);
-console.log(evenDivs[0].parentElement.firstChild);
-console.log(evenDivs[0].parentElement.firstElementChild);
-console.log(evenDivs[0].nextSibling);
-console.log(evenDivs[0].nextElementSibling);
-console.log(evenDivs[0].previousSibling);
-console.log(evenDivs[0].previousElementSibling);
+document.addEventListener("readystatechange", (event) => {
+    if(event.target.readyState === "complete") {
+        console.log("readyState: complete");
+        initApp();
+    }
+});
 
-view1.style.display = "none";
-view2.style.display = "flex";
-view2.style.flexDirection = "row";
-view2.style.flexWrap = "wrap";
-view2.style.margin = "10px";
+const initApp = () => {
+    const view = document.querySelector("#view2");
+    const div = view.querySelector("div");
+    const h2 = div.querySelector("h2");
 
+    view.addEventListener("click", (event) => {
+        view.style.backgroundColor = "purple";
+    });
 
-//to clear out a section and add new content
-while(view2.lastChild){
-    view2.lastChild.remove();
-}
+    div.addEventListener("click", (event) => {
+        div.style.backgroundColor = "blue";
+    });
 
-const createDivs = (parent, iter) => {
-    const newDiv = document.createElement("div");
-    newDiv.textContent = iter;
-    newDiv.style.backgroundColor = "#000";
-    newDiv.style.width = "100px";
-    newDiv.style.height = "100px";
-    newDiv.style.margin = "10px";
-    newDiv.style.display = "flex";
-    newDiv.style.justifyContent = "center";
-    newDiv.style.alignItems = "center";
-    parent.append(newDiv);
-}
+    h2.addEventListener("click", (event) => {
+        event.target.textContent = "Clicked";
+    });
 
-//createDivs(view2, 10);
-
-for (let i=1; i<=12; i++) {
-    createDivs(view2, i);
 }
