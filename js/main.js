e52c58e5dd1a799bed857c0fcfd8f3c282bbacd1
Rafik <s94rfarh@uni-bonn.de>
2024-12-24 02:11:29 +0100

    promises,  how they work and how to get the results of them with thenables

--- js/main.js
+++ js/main.js
@@ -1,21 +1,40 @@
-// Web Storage API
+// Fetch API requires a discussion of ...
+// Callbacks, Promises, Thenables, and Async/Await
 
-// Not part of the DOM - refers to the Window API
-// Available to JS via the globale variable: window
+//Callbacks are functions that are passed to other functions as parameters:
 
-// We do not have to type window. It is implied:
-
-const myArray = ["eat", "sleep", "code"];
-const myObject = {
-    name: "Dave",
-    hobbies: ["eat", "sleep", "code"],
-    logName: function(){
-        console.log(this.name);
-    }
+function firstFunction(parameters, callback){
+    //do stuff
+    callback(); //Shaine of events
 }
 
-localStorage.setItem("mylocalStore", JSON.stringify(myArray));
-const storeLength = localStorage.length;
-const mylocalData = JSON.parse(localStorage.getItem("mylocalStore"));
+//AKA "callback hell", because it could get complicated with anonymous functions, promises can maybe help:
 
-console.log(storeLength);
+// Promises deliver async code, but js is usually snychronous(doing one thing at a time). Promises: you go ahead an I will catch up once I am finsihed with my work
+// -> A promise could be pending while some other js code goes ahead and executes, actually two different block of codes are executed at once
+
+// 3 states: Pending, Fulfilled, Rejected
+
+const myPromise = new Promise((resolve, reject) => {
+    const error = false;
+    if (!error){
+        resolve("Yes! resolved the promise!");
+    }
+    else {
+        reject("No! rejected the promise.");
+    }
+});
+
+const myNextPromise = new Promise((resolve, reject) => {
+    setTimeout(function() {
+        resolve("myNextPromise resolved!")
+    }, 3000);
+});
+
+myNextPromise.then(value => {
+    console.log(value)
+});
+
+myPromise.then(value => {
+    console.log(value)
+});
